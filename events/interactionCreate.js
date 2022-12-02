b6dea60874cf3c9c563148ba7931386e6e6069e7
hungphong0910 <phong.tran@desports.gg>
2022-12-02 11:51:36 +0700

    fix(ladder info): fix data

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -61,38 +61,46 @@ const ladderButtonHandle = async (interaction) => {
 	ladderId = ladderId?.[1];
 	if (ladderId) {
 		let response = await request(`${process.env.API_SERVER}/ladder/details/${ladderId}`);
-		console.log(ladderId, response);
 		let { data } = await response.body.json();
 		let ladderInfo = data?.ladder;
+		let game_mode = "";
+		let attributes = ladderInfo.type === "DOTA2" ? {
+			rank: {
+				from: ladderInfo.game_config?.config?.overall?.tier?.from?.toString(),
+				to: ladderInfo.game_config?.config?.overall?.tier?.to?.toString()
+			},
+			gameMode: ladderInfo.game_config?.config?.match_record_condition?.game_mode?.mode?.map((mode, index) => {
+				if (index == 0) {
+					game_mode = mode?.toString();
+					return;
+				}
+				game_mode = `${game_mode?.toString()}, ${mode?.toString()}`;
+			})
+		} : {
+			rank: {
+				from: ladderInfo.game_config?.config?.overall?.tier?.from?.toString(),
+				to: ladderInfo.game_config?.config?.overall?.tier?.to?.toString()
+			},
+			gameMode: `${ladderInfo.game_config?.config?.match_record_condition?.game_mode?.mode} - ${ladderInfo.game_config?.config?.match_record_condition?.game_mode?.setting} - ${ladderInfo.game_config?.config?.match_record_condition?.game_mode?.category}`
+		};
+
 		let embed = new EmbedBuilder()
       .setColor(0x0099FF)
 			.addFields(
+				{ name: 'Ladder name', value: ladderInfo.title },
 				{ name: 'Game', value: ladderInfo.game_config.game.name },
-				{ name: 'Status', value: ladderInfo.status },
-				{ name: 'Hosted by', value: `${ladderInfo.start_time} - ${ladderInfo.end_time}` },
+				{ name: 'Status', value: ladderInfo.status?.toString() || " No data" },
+				{ name: 'Hosted by', value: ladderInfo.organizer?.toString() || "No data" },
+				{ name: 'Time', value: `${ladderInfo.start_time} - ${ladderInfo.end_time}` },
 				{ name: 'Prizepool', value: ladderInfo.prize_pool?.total_prize?.toString() },
-				{ name: 'Game mode', value: ladderInfo.game_config?.config?.match_record_condition?.game_mode?.mode?.[0]?.toString() || "Mode" },
-				{ name: 'Rank', value: `[Rank] - [Rank]` },
-				{ name: 'Region', value: ladderInfo.game_config?.config?.overall?.region?.[0]?.toString() },
+				{ name: 'Game mode', value: game_mode?.toString() || "No data" },
+				{ name: 'Rank', value: `${attributes.rank.from} - ${attributes.rank.to}` },
+				{ name: 'Region', value: ladderInfo.game_config?.config?.overall?.region?.[0]?.toString() || "No region" },
 				{ name: 'Entry fee', value: ladderInfo.registration_fee?.amount?.toString() },
 				{ name: 'Participants(s)', value: `${ladderInfo.players?.length}/${ladderInfo.max_player}`?.toString() },
 				{ name: 'Prize Distribution', value: `1st [TBA] / 1st [team name]` },
 			)
-			.setFooter({ text: "Join ladder?" })
-		let row = new ActionRowBuilder();
-		row.addComponents(
-			new ButtonBuilder()
-				.setCustomId(`ladder-registration-yes`)
-				.setLabel("Yes")
-				.setStyle(ButtonStyle.Success)
-		)
-		row.addComponents(
-			new ButtonBuilder()
-				.setCustomId(`ladder-registration-no`)
-				.setLabel("No")
-				.setStyle(ButtonStyle.Danger)
-		)
 
-		interaction.reply({embeds: [embed], components: [row]});
+		interaction.reply({embeds: [embed]});
 	}
 }
